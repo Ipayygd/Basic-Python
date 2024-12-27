2e4137342c816019e5ba991238dc3e7e20a8f6a0
ipayygd <ipayygd@gmail.com>
2024-12-27 19:36:50 +0700

    09 - Operator Assigment

---
+++ 09 - Operator Assigment/Main.py
@@ -0,0 +1,105 @@
+# Operasi yang dapat dilakukan dengan penyingkatan
+# Operator =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<=
+a = 5
+print("Nilai a =", a)
+
+# Penjumlahan
+print("===== Penjumlahan")
+# a = a + 1
+# disingkat dengan +=
+a += 1
+
+print("Nilai a =", a)
+
+# Pengurangan
+print("===== Pengurangan")
+# a = a - 1
+# disingkat dengan -=
+a -= 1
+
+print("Nilai a =", a)
+
+# Perkalian
+print("===== Perkalian")
+# a = a * 2
+# disingkat dengan *=
+a *= 2
+
+print("Nilai a =", a)
+
+# Pembagian
+print("===== Pembagian")
+# a = a / 2
+# disingkat dengan /=
+a /= 2
+
+print("Nilai a =", a)
+
+# Modulus
+print("===== Modulus")
+# a = a % 2
+# disingkat dengan %=
+a %= 2
+
+print("Nilai a =", a)
+
+# Floor Division
+print("===== Floor Division")
+# a = a // 2
+# disingkat dengan //=
+a //= 2
+
+print("Nilai a =", a)
+
+# Perpangkatan
+print("===== Perpangkatan")
+# a = a ** 2
+# disingkat dengan **=
+a **= 2
+
+print("Nilai a =", a)
+
+# Bitwise OR
+print("===== Bitwise OR")
+c = True
+# c = True | False
+# disingkat dengan |=
+c |= False
+
+print("Nilai c =", c)
+
+# Bitwise AND
+print("===== Bitwise AND")
+c = True
+# c = True & False
+# disingkat dengan &=
+c &= False
+
+print("Nilai c =", c)
+
+# Bitwise XOR
+print("===== Bitwise XOR")
+c = True
+# c = True ^ False
+# disingkat dengan ^=
+c ^= False
+
+print("Nilai c =", c)
+
+# Bitwise Right Shift
+print("===== Bitwise Right Shift")
+d = 0b0100
+# d = 0b0100 >> 2
+# disingkat dengan >>= 
+d >>= 2
+
+print("Nilai d =", format(d, "04b"))
+
+# Bitwise Left Shift
+print("===== Bitwise Left Shift")
+d = 0b0100
+# d = 0b0100 << 1
+# disingkat dengan <<=
+d <<= 1
+
+print("Nilai d =", format(d, "04b"))
