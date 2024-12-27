9b32c69d6f360304ad60791911b78f8c42c32e7c
ipayygd <ipayygd@gmail.com>
2024-12-27 18:10:17 +0700

    06 - Operasi Komparasi

---
+++ 06 - Operasi Komparasi/Main.py
@@ -0,0 +1,29 @@
+# Operasi Komparasi (>, <, ==, !=, >=, <=, is, is not)
+# Setiap hasil operasi komparasi akan bernilai boolean, (True/False)
+
+a = 10
+b = 5
+
+# Lebih besar (>)
+hasil = a > b
+print(a, ">", b, "=", hasil)
+
+# Lebih kecil (<)
+hasil = a < b
+print(a, "<", b, "=", hasil)
+
+# Sama dengan (==)
+hasil = a == b
+print(a, "==", b, "=", hasil)
+
+# Tidak sama dengan (!=)
+hasil = a != b
+print(a, "!=", b, "=", hasil)
+
+# Lebih besar sama dengan (>=)
+hasil = a >= b
+print(a, ">=", b, "=", hasil)
+
+# Lebih kecil sama dengan (<=)
+hasil = a <= b
+print(a, "<=", b, "=", hasil)
