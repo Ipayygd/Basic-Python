492fdf0135c8d53cd0c61a134c47d478259ff64d
ipayygd <ipayygd@gmail.com>
2024-12-27 16:12:44 +0700

    Add : 01 - Variabel

---
+++ 01 - Variabel/Main.py
@@ -0,0 +1,21 @@
+# Variable adalah tempat menyimpan data
+
+# Memasukan nilai
+nama = "Luca"
+umur = 16
+menikah = False
+
+print("Umur =", umur)
+
+# Penamaan
+nilai_mata_kuliah = 10
+kelas2 = 10
+umurSiswa  = 10
+
+# Pemanggilan kedua
+print("Umur =", umur) # Output: Umur = 16 "Karena belum diubah/diperbarui"
+umur = 20
+print("Umur =", umur) # Output: Umur = 20 "Karena telah diubah/diperbarui"
+
+# Assignment Indirect
+umurSiswa = umur
