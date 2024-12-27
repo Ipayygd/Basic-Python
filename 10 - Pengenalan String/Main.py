49951f5ea354b870bc5dbec924a6b84049cc8aeb
ipayygd <ipayygd@gmail.com>
2024-12-27 20:01:09 +0700

    10 - Pengenalan String

---
+++ 10 - Pengenalan String/Main.py
@@ -0,0 +1,34 @@
+# Cara-cara membuat String
+
+data = "Ini adalah string"
+print(data)
+print(type(data))
+
+# 1. Dengan menggunakan single quote (')
+data = 'Menggunakan single quote'
+print(data)
+
+# 2. Dengan menggunakan double quote (")
+data = "Menggunakan double quote"
+print(data)
+
+# 3. Dengan menggunakan triple quote (""")
+data = """Menggunakan triple quote
+dengan baris baru"""
+print(data)
+
+# 4. Dengan menggunakan backslash (\)
+data = "Hari ini adalah Jum\'at"
+print(data)
+
+# 5. String Literal
+print("C:\new Folder") # Salah
+print(r"C:\New Folder") # Benar (Menggunakan raw string)
+
+# 6. String Multiline
+data = """Menggunakan,
+Multiline String"""
+print(data)
+
+
+# Masih banyak lagi...
